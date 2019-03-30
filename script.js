d4a897e6643a4de276bc890915623d5114e50955
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2019-03-29 21:54:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(Document).ready(function(){
+$(".red").click(function(){
+    $(".red").fadeOut();
+    });
+    
+    $(".green").click(function(){
+        $(".blue").hide();
+    });
+
+    $(".yellow").click(function(){
+        $(".blue").fadeIn();
+        $(".green").hide();
+    });
+    $(".blue").click(function(){
+        $(".red, .green, .yellow").toggle();
+    });
+
+});
